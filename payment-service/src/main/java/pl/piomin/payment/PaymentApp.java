71cc4482b56983233766a9d04f45ab3d2d9cc691
piomin <piotr.minkowski@gmail.com>
2022-02-10 13:10:31 +0100

    adding datafaker

--- payment-service/src/main/java/pl/piomin/payment/PaymentApp.java
+++ payment-service/src/main/java/pl/piomin/payment/PaymentApp.java
@@ -1,5 +1,6 @@
 package pl.piomin.payment;
 
+import net.datafaker.Faker;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -43,9 +44,10 @@ public class PaymentApp {
     @PostConstruct
     public void generateData() {
         Random r = new Random();
+        Faker faker = new Faker();
         for (int i = 0; i < 100; i++) {
             int count = r.nextInt(1000);
-            Customer c = new Customer(null, "Customer" + i, count, 0);
+            Customer c = new Customer(null, faker.name().fullName(), count, 0);
             repository.save(c);
         }
     }
